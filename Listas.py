6594e03475fa26f2c33c2c1cd9a3fbb13afab2d9
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-09 10:34:57 -0300

    Adiciona estudos sobre listas em python

---
+++ Listas.py
@@ -0,0 +1,39 @@
+# Lista: Sequencia de valores - Passíveis de mudança.
+lista_Interiros = [1, 2, 3, 4, 5, 6, 7]
+
+# Somar valores da lista
+sum(lista_Interiros)
+
+# Maior valor da lsita
+max(lista_Interiros)
+
+# Menor valor da lsita
+min(lista_Interiros)
+
+# Se elemento está contido na lista
+esta_Contido = 1 in lista_Interiros
+
+# Percorrer elementos da lista]
+for elemento_Inteiro in lista_Interiros:
+    print(elemento_Inteiro)
+
+# Operar todos os valores da lista
+lista_Interiros *= 1
+
+# Incluir elemento em uma lista
+lista_Interiros.append(8)
+
+# Retirar ultimo elemento da lista
+lista_Interiros.pop()
+
+# Retirar elemento de uma posição específica da lista
+lista_Interiros.pop(0)
+
+# Remover um elemento específico de uma lista
+lista_Interiros.remove(2)
+
+# Ordenar uma lista - Ascendente
+lista_Interiros.sort()
+
+# Ordenar uma lista - Descendente
+lista_Interiros.reverse()
