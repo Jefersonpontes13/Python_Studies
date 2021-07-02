b20333332a8cdd803abf98ebdce4912d2b7c8ac9
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-07-01 21:44:55 -0300

    Adiciona indexação em Listas.py

--- Listas.py
+++ Listas.py
@@ -1,6 +1,11 @@
 # type[list] Lista: Sequencia de valores - Passíveis de mudança.
 lista_Interiros = [1, 2, 3, 4, 5, 6, 7]
 
+# Seleciana valores da lista por indexação
+print(lista_Interiros[:3])
+
+# Seleciona elementos por meio de um step
+print(lista_Interiros[::2]) # Nesse caso retorna os impares
 # Somar valores da lista
 sum(lista_Interiros)
 
@@ -40,3 +45,5 @@ lista_Interiros.reverse()
 
 # Python - List Comprehension
 lista_Interiros_Ao_Quadrado = [elemento_Inteiro ** 2 for elemento_Inteiro in lista_Interiros]
+
+
