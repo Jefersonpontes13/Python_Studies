167655d501feedf1d013471be70efb8b0e61d9f8
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-07-01 21:47:59 -0300

    Adiciona declaração de tupla sem parenteses

--- Tuplas.py
+++ Tuplas.py
@@ -1,6 +1,7 @@
 # type[tuple] Tupla: Sequencia de valores - Não passíveis de mudança.
 
 tupla_Inteiros = (1, 2, 3, 4, 5, 6, 7)
+# tupla_Inteiros = 1, 2, 3, 4, 5, 6, 7
 
 # Converter lista em uma tupla
 tupla_Inteiros_copy = tuple([1, 2, 3, 4, 5, 6, 7])
