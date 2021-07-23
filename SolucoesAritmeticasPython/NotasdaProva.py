49fa696f9356733069aae5944bfe3c507f0dba1d
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-07-23 09:33:02 -0300

    Adiciona Soluções Aritméticas
    NotasdaProva.py

---
+++ SolucoesAritmeticasPython/NotasdaProva.py
@@ -0,0 +1,20 @@
+
+
+def main():
+
+    nota = int(input())
+
+    if nota == 0:
+        print('E')
+    elif 1 <= nota <= 35:
+        print('D')
+    elif 36 <= nota <= 60:
+        print('C')
+    elif 61 <= nota <= 85:
+        print('B')
+    elif 86 <= nota <= 100:
+        print('A')
+
+
+if __name__ == '__main__':
+    main()
